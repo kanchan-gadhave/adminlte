7a0f731549edad37c36a450ed8b558940aa0b787
kanchan-gadhave <kanchang@proschoolonline.com>
2019-11-08 12:11:36 +0530

    Update footer.blade.php
    test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -31,8 +31,62 @@
 <script src="public/plugins/jquery-mapael/maps/usa_states.min.js"></script>
 <!-- ChartJS -->
 <script src="public/plugins/chart.js/Chart.min.js"></script>
+<!-- DataTables -->
+<script src="public/plugins/datatables/jquery.dataTables.js"></script>
+<script src="public/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
 
 <!-- PAGE SCRIPTS -->
 <script src="public/dist/js/pages/dashboard2.js"></script>
 
+<script>
+  $(function () {
+	  
+      $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+</script>
+<script>
+  $(function () {
+    //Enable check and uncheck all functionality
+    $('.checkbox-toggle').click(function () {
+      var clicks = $(this).data('clicks')
+      if (clicks) {
+        //Uncheck all checkboxes
+        $('.mailbox-messages input[type=\'checkbox\']').prop('checked', false)
+        $('.checkbox-toggle .far.fa-check-square').removeClass('fa-check-square').addClass('fa-square')
+      } else {
+        //Check all checkboxes
+        $('.mailbox-messages input[type=\'checkbox\']').prop('checked', true)
+        $('.checkbox-toggle .far.fa-square').removeClass('fa-square').addClass('fa-check-square')
+      }
+      $(this).data('clicks', !clicks)
+    })
+
+    //Handle starring for glyphicon and font awesome
+    $('.mailbox-star').click(function (e) {
+      e.preventDefault()
+      //detect type
+      var $this = $(this).find('a > i')
+      var glyph = $this.hasClass('glyphicon')
+      var fa    = $this.hasClass('fa')
+
+      //Switch states
+      if (glyph) {
+        $this.toggleClass('glyphicon-star')
+        $this.toggleClass('glyphicon-star-empty')
+      }
+
+      if (fa) {
+        $this.toggleClass('fa-star')
+        $this.toggleClass('fa-star-o')
+      }
+    })
+  })
+</script>
 <html>
